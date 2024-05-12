77178959b39cf86b59a620ef204dc2a24b942908
luizlealdev <luizandreleal100@gmail.com>
2024-05-12 17:47:29 -0300

    data class for notes

---
+++ app/src/main/java/dev/luizleal/mynotes/model/Note.kt
@@ -0,0 +1,15 @@
+package dev.luizleal.mynotes.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "notes")
+@Parcelize
+data class Note(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val noteTitle: String,
+    val noteDescription: String,
+    val noteLastChangeData: String
+) : Parcelable
