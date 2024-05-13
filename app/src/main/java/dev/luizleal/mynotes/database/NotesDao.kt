8ed76a4085a8adc76bd7338ab70306f8a77d1d1f
luizlealdev <luizandreleal100@gmail.com>
2024-05-12 21:46:06 -0300

    NotesDao.kt created

---
+++ app/src/main/java/dev/luizleal/mynotes/database/NotesDao.kt
@@ -0,0 +1,29 @@
+package dev.luizleal.mynotes.database
+
+import android.provider.ContactsContract.CommonDataKinds.Note
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface NotesDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertNote(note: Note)
+
+    @Update
+    suspend fun updateNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+
+    @Query("SELECT * FROM notes ORDER BY id DESC")
+    fun getAllNotes(): LiveData<List<Note>>
+
+    @Query("SELECT * FROM notes WHERE noteTitle LIKE :query OR noteDescription LIKE :query")
+    fun searchNoteQuery(query: String?): LiveData<List<Note>>
+
+}
