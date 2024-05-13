a1ad6c91c3c5b31e6e608a69c1e8a619f81b17dc
luizlealdev <luizandreleal100@gmail.com>
2024-05-13 18:51:49 -0300

    NoteDatabase for manage the database instance

---
+++ app/src/main/java/dev/luizleal/mynotes/database/NoteDatabase.kt
@@ -0,0 +1,29 @@
+package dev.luizleal.mynotes.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import dev.luizleal.mynotes.model.Note
+
+@Database(entities = [Note::class], version = 1)
+abstract class NoteDatabase : RoomDatabase() {
+    abstract fun getNoteDao(): NotesDao
+
+    companion object {
+        @Volatile
+        private var instance: NoteDatabase? = null
+
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: createDatabase(context).also {
+                instance = it
+            }
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(context.applicationContext, NoteDatabase::class.java, "note_db")
+                .build()
+    }
+}
