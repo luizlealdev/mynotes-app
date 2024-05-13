4173c461efa8ad3250eacdb012125d0dd4790afb
luizlealdev <luizandreleal100@gmail.com>
2024-05-13 19:33:17 -0300

    feat: fab action added

--- app/src/main/java/dev/luizleal/mynotes/fragments/HomeFragment.kt
+++ app/src/main/java/dev/luizleal/mynotes/fragments/HomeFragment.kt
@@ -5,17 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import dev.luizleal.mynotes.R
+import dev.luizleal.mynotes.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
+    private var homeBinding: FragmentHomeBinding? = null
+    private val binding get() = homeBinding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        homeBinding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.fabNewNote.setOnClickListener {
+            it.findNavController().navigate(R.id.action_homeFragment_to_newNoteFragment)
+        }
     }
 
 }
