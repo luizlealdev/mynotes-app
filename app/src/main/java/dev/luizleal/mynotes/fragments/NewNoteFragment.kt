03c92657eb9824b08ca7b89d3d18158fa15f8b8f
luizlealdev <luizandreleal100@gmail.com>
2024-05-18 19:59:15 -0300

    feat: screen for add new notes are working now

--- app/src/main/java/dev/luizleal/mynotes/fragments/NewNoteFragment.kt
+++ app/src/main/java/dev/luizleal/mynotes/fragments/NewNoteFragment.kt
@@ -5,15 +5,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import dev.luizleal.mynotes.MainActivity
 import dev.luizleal.mynotes.R
 import dev.luizleal.mynotes.databinding.FragmentNewNoteBinding
+import dev.luizleal.mynotes.model.Note
+import dev.luizleal.mynotes.viewModel.NoteViewModel
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
-class NewNoteFragment : Fragment() {
+class NewNoteFragment : Fragment(R.layout.fragment_new_note) {
     private var newNoteBinding: FragmentNewNoteBinding? = null
     private val binding get() = newNoteBinding!!
+
+    private lateinit var notesViewModel: NoteViewModel
+    private lateinit var newNoteView: View
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,11 +38,35 @@ class NewNoteFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.buttonGoBackIcon.setOnClickListener(handleGoBack(requireContext()))
+
+        binding.fabNewNote.setOnClickListener {
+            saveNote(newNoteView)
+        }
+
+        notesViewModel = (activity as MainActivity).noteViewModel
+
+        newNoteView = view
     }
 
     override fun onDestroy() {
         super.onDestroy()
         newNoteBinding = null
+    }
+
+    private fun saveNote(view: View) {
+        val noteTitle = binding.editNoteTitle.text.toString().trim()
+        val noteDescription = binding.editNoteDescription.text.toString().trim()
+        val noteLastChangeDate = getCurrentDate()
+
+        if (noteTitle.isNotEmpty()) {
+            val note = Note(0, noteTitle, noteDescription, noteLastChangeDate)
+            notesViewModel.addNote(note)
+
+            Toast.makeText(newNoteView.context, "Note saved", Toast.LENGTH_SHORT).show()
+            view.findNavController().popBackStack(R.id.homeFragment, false)
+        } else
+            Toast.makeText(newNoteView.context, "Please enter the note title", Toast.LENGTH_SHORT)
+                .show()
     }
 
     private fun handleGoBack(context: Context): View.OnClickListener {
@@ -46,6 +80,12 @@ class NewNoteFragment : Fragment() {
                 .setNegativeButton(getString(R.string.no), null)
                 .show()
         }
+    }
+
+    private fun getCurrentDate(): String {
+        val currentDate = Calendar.getInstance()
+        val dateFormat = SimpleDateFormat("d MMM", Locale.getDefault())
+        return dateFormat.format(currentDate.time)
     }
 
 }
