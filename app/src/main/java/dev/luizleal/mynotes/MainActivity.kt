9183d7a576c0d79ec67055d728c2f8ad01ab48ec
luizlealdev <luizandreleal100@gmail.com>
2024-05-18 09:05:58 -0300

    feat: setupViewModel

--- app/src/main/java/dev/luizleal/mynotes/MainActivity.kt
+++ app/src/main/java/dev/luizleal/mynotes/MainActivity.kt
@@ -5,8 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import dev.luizleal.mynotes.database.NoteDatabase
+import dev.luizleal.mynotes.repository.NoteRepository
+import dev.luizleal.mynotes.viewModel.NoteViewModel
+import dev.luizleal.mynotes.viewModel.NoteViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+    lateinit var noteViewModel: NoteViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setupViewModel()
+    }
+
+    fun setupViewModel() {
+        val noteRepository = NoteRepository(NoteDatabase(this))
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
     }
 }
--- app/src/main/java/dev/luizleal/mynotes/fragments/HomeFragment.kt
+++ app/src/main/java/dev/luizleal/mynotes/fragments/HomeFragment.kt
@@ -1,18 +1,27 @@
 package dev.luizleal.mynotes.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import dev.luizleal.mynotes.MainActivity
 import dev.luizleal.mynotes.R
+import dev.luizleal.mynotes.adapter.NoteAdapter
 import dev.luizleal.mynotes.databinding.FragmentHomeBinding
+import dev.luizleal.mynotes.model.Note
+import dev.luizleal.mynotes.viewModel.NoteViewModel
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private var homeBinding: FragmentHomeBinding? = null
     private val binding get() = homeBinding!!
+
+    private lateinit var notesViewModel: NoteViewModel
+    private lateinit var noteAdapter: NoteAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,8 +35,55 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        notesViewModel = (activity as MainActivity).noteViewModel
+
         binding.fabNewNote.setOnClickListener {
             it.findNavController().navigate(R.id.action_homeFragment_to_newNoteFragment)
+        }
+        searchNote()
+        setupHomeRecyclerView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        homeBinding = null
+    }
+
+    private fun updateUI(note: List<Note>?) {
+        if (note != null) {
+            if (note.isNotEmpty()) {
+                binding.textNoNotesAlert.visibility = View.GONE
+                binding.recyclerviewNotes.visibility = View.VISIBLE
+            } else {
+                binding.textNoNotesAlert.visibility = View.VISIBLE
+                binding.recyclerviewNotes.visibility = View.GONE
+            }
+        }
+    }
+
+    private fun setupHomeRecyclerView() {
+        noteAdapter = NoteAdapter()
+        binding.recyclerviewNotes.apply {
+            layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+            setHasFixedSize(true)
+            adapter = noteAdapter
+        }
+
+        activity?.let {
+            notesViewModel.getAllNotes().observe(viewLifecycleOwner) { note ->
+                noteAdapter.differ.submitList(note)
+                updateUI(note)
+            }
+        }
+    }
+
+    private fun searchNote() {
+        binding.editNoteSearch.addTextChangedListener { text ->
+            text?.let {
+                notesViewModel.searchNote(it.toString()).observe(viewLifecycleOwner) { list ->
+                    noteAdapter.differ.submitList(list)
+                }
+            }
         }
     }
 
