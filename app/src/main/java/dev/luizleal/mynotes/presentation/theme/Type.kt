4d730d6ab5a04181f935896c0cb3b1b8438072ee
luizlealdev <luizandreleal100@gmail.com>
2025-10-27 10:12:01 -0300

    [feat]: update typography styles

--- app/src/main/java/dev/luizleal/mynotes/presentation/theme/Type.kt
+++ app/src/main/java/dev/luizleal/mynotes/presentation/theme/Type.kt
@@ -31,21 +31,19 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Bold,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
+        fontFamily = fontFamily,
+        fontWeight = FontWeight.Normal,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
