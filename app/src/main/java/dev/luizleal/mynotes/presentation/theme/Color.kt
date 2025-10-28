79f83b08271a008a47f4e26992271b714332dd76
luizlealdev <luizandreleal100@gmail.com>
2025-10-28 10:15:55 -0300

    [refactor]: update light theme color

--- app/src/main/java/dev/luizleal/mynotes/presentation/theme/Color.kt
+++ app/src/main/java/dev/luizleal/mynotes/presentation/theme/Color.kt
@@ -5,7 +5,7 @@ import androidx.compose.ui.graphics.Color
 val PrimaryColor = Color(0xFF2C6DF0)
 
 val BackgroundPrimaryLight = Color(0xFFF9F9F9)
-val BackgroundSecondaryLight = Color(0xFFF9F9F9)
+val BackgroundSecondaryLight = Color(0xFFFFFFFF)
 val BackgroundTertiaryLight = Color(0xFFFAFAFA)
 val TextPrimaryLight = Color(0xFF030507)
 val TextSecondaryLight = Color(0xFF6F7173)
