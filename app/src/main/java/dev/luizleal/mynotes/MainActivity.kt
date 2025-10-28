a5b2263167a6c505ab4ee5a67ea8fe04e5a2d561
luizlealdev <luizandreleal100@gmail.com>
2025-10-28 20:44:47 -0300

    [feat]: set up home screen
    Set a space between the statusbar and the screen content

--- app/src/main/java/dev/luizleal/mynotes/MainActivity.kt
+++ app/src/main/java/dev/luizleal/mynotes/MainActivity.kt
@@ -4,13 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import dev.luizleal.mynotes.presentation.screens.home.HomeScreen
 import dev.luizleal.mynotes.presentation.theme.MyNotesTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +18,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyNotesTheme {
-
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .statusBarsPadding()
+                        .navigationBarsPadding()
+                ) {
+                    HomeScreen()
+                }
             }
         }
     }
