2977e049254208fc753bc11dbd8d143af2ba9b50
luizlealdev <luizandreleal100@gmail.com>
2025-10-29 16:00:13 -0300

    [build]: add navigation and serialization dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -52,6 +53,12 @@ dependencies {
 
     //Google Fonts
     implementation(libs.androidx.compose.ui.text.google.fonts)
+
+    //Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    //Serialization
+    implementation(libs.kotlinx.serialization.json)
 
     testImplementation(libs.junit)
 
