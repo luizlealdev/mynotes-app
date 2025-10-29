f1cbc378822b99bd8de05c3f5b0f5df446904595
luizlealdev <luizandreleal100@gmail.com>
2025-10-28 21:10:00 -0300

    [feat]: implement floating action button to home screen

--- app/src/main/java/dev/luizleal/mynotes/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/dev/luizleal/mynotes/presentation/screens/home/HomeScreen.kt
@@ -10,15 +10,35 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import dev.luizleal.mynotes.presentation.components.FloatingActionButton
 import dev.luizleal.mynotes.presentation.components.SearchBar
 import dev.luizleal.mynotes.presentation.theme.MyNotesTheme
 
 @Composable
 fun HomeScreen() {
-    Scaffold { innerPadding ->
+
+    var isFabExpanded by remember { mutableStateOf(false) }
+
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(
+                expanded = isFabExpanded,
+                onFabClick = {
+                    isFabExpanded = !isFabExpanded
+                },
+                onOptionClick = { option ->
+
+                }
+            )
+        }
+    ) { innerPadding ->
         Column(
             modifier = Modifier
                 .consumeWindowInsets(innerPadding)
