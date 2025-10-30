8531879b1fdc985e8df9da532c9ea342547cdda9
luizlealdev <luizandreleal100@gmail.com>
2025-10-30 20:17:42 -0300

    [build]: add room and ksp dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -59,6 +60,11 @@ dependencies {
 
     //Serialization
     implementation(libs.kotlinx.serialization.json)
+
+    //Room Database
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.room.runtime)
+    ksp(libs.androidx.room.compiler)
 
     testImplementation(libs.junit)
 
