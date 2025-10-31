a077cb81f67bf0e789a42ba58d1e49c7319974be
luizlealdev <luizandreleal100@gmail.com>
2025-10-31 10:53:20 -0300

    [feat]: add more folder colors
    - Add RED, YELLOW, INDIGO, and VIOLET to the `FolderColor` enum.
    - Reorder the colors to follow the rainbow spectrum.

--- app/src/main/java/dev/luizleal/mynotes/domain/model/FolderColor.kt
+++ app/src/main/java/dev/luizleal/mynotes/domain/model/FolderColor.kt
@@ -1,8 +1,12 @@
 package dev.luizleal.mynotes.domain.model
 
 enum class FolderColor(val hex: Long) {
-    BLUE(0xFF2E6DEE),
-    GREEN(0xFF2FAC2B),
-    PURPLE(0xFF914AC6),
-    ORANGE(0xFFFF5A59)
+
+    RED(0xFFFF0000),
+    ORANGE(0xFFFF5A59), // Cor fornecida no original
+    YELLOW(0xFFFFFF00),
+    GREEN(0xFF2FAC2B), // Cor fornecida no original
+    BLUE(0xFF2E6DEE), // Cor fornecida no original
+    INDIGO(0xFF4B0082),
+    VIOLET(0xFF914AC6) // Cor roxa/violeta semelhante à fornecida no original
 }
