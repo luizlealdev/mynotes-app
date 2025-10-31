434d3cb8b8ea6e50b4e50a6dc761b74f1d2df302
luizlealdev <luizandreleal100@gmail.com>
2025-10-31 15:13:57 -0300

    [refactor]: update repositories interfaces to return Flow

--- app/src/main/java/dev/luizleal/mynotes/domain/repository/FolderRepository.kt
+++ app/src/main/java/dev/luizleal/mynotes/domain/repository/FolderRepository.kt
@@ -1,11 +1,12 @@
 package dev.luizleal.mynotes.domain.repository
 
 import dev.luizleal.mynotes.domain.model.Folder
+import kotlinx.coroutines.flow.Flow
 
 interface FolderRepository {
 
     suspend fun insertFolder(folder: Folder)
     suspend fun updateFolder(folder: Folder)
     suspend fun deleteFolderFolder(folder: Folder)
-    fun getAllFolders(): List<Folder?>
+    fun getAllFolders(): Flow<List<Folder?>>
 }
--- app/src/main/java/dev/luizleal/mynotes/domain/repository/NoteRepository.kt
+++ app/src/main/java/dev/luizleal/mynotes/domain/repository/NoteRepository.kt
@@ -1,14 +1,15 @@
 package dev.luizleal.mynotes.domain.repository
 
 import dev.luizleal.mynotes.domain.model.Note
+import kotlinx.coroutines.flow.Flow
 
 interface NoteRepository {
 
     suspend fun insertNote(note: Note)
     suspend fun updateNote(note: Note)
     suspend fun deleteNote(note: Note)
-    fun getAllNotes(): List<Note?>
-    fun getNoteById(id: Long): Note?
-    fun getNotesByFolderId(id: Long): List<Note?>
-    fun searchNotes(query: String): List<Note?>
+    fun getAllNotes(): Flow<List<Note?>>
+    fun getNoteById(id: Long): Flow<Note?>
+    fun getNotesByFolderId(id: Long): Flow<List<Note?>>
+    fun searchNotes(query: String): Flow<List<Note?>>
 }
