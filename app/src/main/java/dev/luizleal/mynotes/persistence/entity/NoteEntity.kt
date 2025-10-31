166734c6b752e9e7c562d6dbb1aafd3ce48a7207
luizlealdev <luizandreleal100@gmail.com>
2025-10-31 09:30:55 -0300

    [feat]: add foreign key to NoteEntity
    - Add foreign key to FolderEntity
    - Update column names
    - Remove `folder` property in favor of `folderId`

--- app/src/main/java/dev/luizleal/mynotes/persistence/entity/NoteEntity.kt
+++ app/src/main/java/dev/luizleal/mynotes/persistence/entity/NoteEntity.kt
@@ -1,16 +1,30 @@
 package dev.luizleal.mynotes.persistence.entity
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
+import dev.luizleal.mynotes.util.Constants
 import java.util.Date
 
-@Entity
+@Entity(
+    tableName = Constants.NOTE_TABLE,
+    foreignKeys = [
+        ForeignKey(
+            entity = FolderEntity::class,
+            parentColumns = ["id"],
+            childColumns = ["folder_id"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index("folder_id")]
+)
 data class NoteEntity(
-    @PrimaryKey(autoGenerate = true)
-    val id: Long = 0,
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
     val title: String,
     val content: String,
-    val createdAt: Date = Date(),
-    val updatedAt: Date? = null,
-    val folder: FolderEntity? = null
+    @ColumnInfo(name ="created_at") val createdAt: Date = Date(),
+    @ColumnInfo(name = "updated_at") val updatedAt: Date? = null,
+    @ColumnInfo(name = "folder_id") val folderId: Long? = null,
 )
