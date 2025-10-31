748169170e4175c70406fe08282bee0a1d5dae1d
luizlealdev <luizandreleal100@gmail.com>
2025-10-31 10:51:58 -0300

    [refactor]: update repository interfaces to use domain models
    - Change `FolderRepository` to use the `Folder` domain model instead of `FolderEntity`.
    - Change `NoteRepository` to use the `Note` domain model instead of `NoteEntity`.
    - Update the `getAllFolders` function signature in `FolderRepository`.

--- app/src/main/java/dev/luizleal/mynotes/domain/repository/FolderRepository.kt
+++ app/src/main/java/dev/luizleal/mynotes/domain/repository/FolderRepository.kt
@@ -1,11 +1,11 @@
 package dev.luizleal.mynotes.domain.repository
 
-import dev.luizleal.mynotes.persistence.entity.FolderEntity
+import dev.luizleal.mynotes.domain.model.Folder
 
 interface FolderRepository {
 
-    suspend fun insertFolder(folder: FolderEntity)
-    suspend fun updateFolder(folder: FolderEntity)
-    suspend fun deleteFolderFolder(folder: FolderEntity)
-    fun getAllFolders(folder: FolderEntity)
+    suspend fun insertFolder(folder: Folder)
+    suspend fun updateFolder(folder: Folder)
+    suspend fun deleteFolderFolder(folder: Folder)
+    fun getAllFolders(): List<Folder?>
 }
--- app/src/main/java/dev/luizleal/mynotes/domain/repository/NoteRepository.kt
+++ app/src/main/java/dev/luizleal/mynotes/domain/repository/NoteRepository.kt
@@ -1,12 +1,12 @@
 package dev.luizleal.mynotes.domain.repository
 
-import dev.luizleal.mynotes.persistence.entity.NoteEntity
+import dev.luizleal.mynotes.domain.model.Note
 
 interface NoteRepository {
 
-    suspend fun insertNote(note: NoteEntity)
-    suspend fun updateNote(note: NoteEntity)
-    suspend fun deleteNote(note: NoteEntity)
+    suspend fun insertNote(note: Note)
+    suspend fun updateNote(note: Note)
+    suspend fun deleteNote(note: Note)
     fun getAllNotes()
     fun getNoteById(id: Long)
     fun getNotesByFolderId(id: Long)
