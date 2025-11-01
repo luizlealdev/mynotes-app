3731e19bbc1835afa980adc2bc18d7dfa46bf4ce
luizlealdev <luizandreleal100@gmail.com>
2025-11-01 10:51:22 -0300

    [feat]: enable Hilt dependency injection for MainActivity
    - Add `@AndroidEntryPoint` annotation to `MainActivity` to allow for dependency injection.
    - Update `safeDrawingPadding` modifier call to remove explicit `Companion` reference.

--- app/src/main/java/dev/luizleal/mynotes/presentation/MainActivity.kt
+++ app/src/main/java/dev/luizleal/mynotes/presentation/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
 import dev.luizleal.mynotes.presentation.navigation.AppNavHost
 import dev.luizleal.mynotes.presentation.theme.MyNotesTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyNotesTheme {
                 Box(
-                    modifier = Modifier.Companion.safeDrawingPadding()
+                    modifier = Modifier.safeDrawingPadding()
                 ) {
                     AppNavHost()
                 }
