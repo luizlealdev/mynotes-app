de6366af439963b9698022df2c70e9bdea8efbf5
luizlealdev <luizandreleal100@gmail.com>
2025-11-01 10:02:21 -0300

    [feat]: create NoteViewModel

---
+++ app/src/main/java/dev/luizleal/mynotes/presentation/viewmodel/NoteViewModel.kt
@@ -0,0 +1,184 @@
+package dev.luizleal.mynotes.presentation.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.luizleal.mynotes.domain.model.Note
+import dev.luizleal.mynotes.domain.repository.NoteRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class NoteViewModel(
+    private val repository: NoteRepository
+) : ViewModel() {
+
+    private val _noteListState = MutableStateFlow(NoteState<List<Note>>())
+    val noteListState = _noteListState.asStateFlow()
+
+    private val _noteState = MutableStateFlow(NoteState<Note>())
+    val noteState = _noteState.asStateFlow()
+
+    init {
+        getAllNotes()
+    }
+
+    fun getAllNotes() = viewModelScope.launch {
+        _noteState.update { state ->
+            state.copy(isLoading = true)
+        }
+
+        try {
+            repository.getAllNotes().collect { notes ->
+                _noteListState.update { state ->
+                    state.copy(
+                        data = notes,
+                        isLoading = false
+                    )
+                }
+            }
+        } catch (e: Exception) {
+            Log.e("Get all notes", "Error getting notes: $e")
+            _noteListState.update { state ->
+                state.copy(error = e.message, isLoading = false)
+            }
+
+            return@launch
+        }
+    }
+
+    fun getNoteById(id: Long) = viewModelScope.launch {
+        _noteState.update { state ->
+            state.copy(isLoading = true)
+        }
+
+        try {
+            repository.getNoteById(id).collect { note ->
+                _noteState.update { state ->
+                    state.copy(
+                        data = note,
+                        isLoading = false
+                    )
+                }
+            }
+        } catch (e: Exception) {
+            Log.e("Get note by ID", "Error getting note: $e")
+            _noteListState.update { state ->
+                state.copy(error = e.message, isLoading = false)
+            }
+
+            return@launch
+        }
+    }
+
+    fun getNotesByFolderId(id: Long) = viewModelScope.launch {
+        _noteListState.update { state ->
+            state.copy(isLoading = true)
+        }
+
+        try {
+            repository.getNotesByFolderId(id).collect { notes ->
+                _noteListState.update { state ->
+                    state.copy(
+                        data = notes,
+                        isLoading = false
+                    )
+                }
+            }
+        } catch (e: Exception) {
+            Log.e("Get notes by folder ID", "Error getting notes: $e")
+            _noteListState.update { state ->
+                state.copy(error = e.message, isLoading = false)
+            }
+
+            return@launch
+        }
+    }
+
+    fun searchNotes(query: String) = viewModelScope.launch {
+        _noteListState.update { state ->
+            state.copy(isLoading = true)
+        }
+
+        try {
+            repository.searchNotes(query).collect { notes ->
+                _noteListState.update { state ->
+                    state.copy(
+                        data = notes,
+                        isLoading = false
+                    )
+                }
+            }
+        } catch (e: Exception) {
+            Log.e("Search notes", "Error searching notes: $e")
+            _noteListState.update { state ->
+                state.copy(error = e.message, isLoading = false)
+            }
+
+            return@launch
+        }
+    }
+
+    fun insertNote(title: String, content: String) = viewModelScope.launch {
+        _noteListState.update { state ->
+            state.copy(isLoading = true)
+        }
+
+        try {
+            val note = Note(
+                title = title,
+                content = content
+            )
+            repository.insertNote(note)
+        } catch (e: Exception) {
+            Log.e("Insert note", "Error inserting note: $e")
+            _noteListState.update { state ->
+                state.copy(error = e.message, isLoading = false)
+            }
+
+            return@launch
+        }
+    }
+
+    fun updateNote(note: Note) = viewModelScope.launch {
+        _noteListState.update { state ->
+            state.copy(isLoading = true)
+        }
+
+        try {
+            repository.updateNote(note)
+        } catch (e: Exception) {
+            Log.e("Update note", "Error updating note: $e")
+            _noteListState.update { state ->
+                state.copy(error = e.message, isLoading = false)
+            }
+
+            return@launch
+        }
+    }
+
+    fun deleteNote(note: Note) = viewModelScope.launch {
+        _noteListState.update { state ->
+            state.copy(isLoading = true)
+        }
+
+        try {
+            repository.deleteNote(note)
+        } catch (e: Exception) {
+            Log.e("Delete note", "Error deleting note: $e")
+            _noteState.update { state ->
+                state.copy(error = e.message, isLoading = false)
+            }
+
+            return@launch
+        }
+    }
+
+}
+
+data class NoteState<T>(
+    val isLoading: Boolean = false,
+    val data: T? = null,
+    val error: String? = null
+)
