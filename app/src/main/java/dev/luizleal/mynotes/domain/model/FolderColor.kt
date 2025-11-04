808406eb76782c4359a7b785cafa242f44927049
luizlealdev <luizandreleal100@gmail.com>
2025-11-03 21:00:20 -0300

    [feat]: add new colors to `FolderColor` enum

--- app/src/main/java/dev/luizleal/mynotes/domain/model/FolderColor.kt
+++ app/src/main/java/dev/luizleal/mynotes/domain/model/FolderColor.kt
@@ -2,11 +2,13 @@ package dev.luizleal.mynotes.domain.model
 
 enum class FolderColor(val hex: Long) {
 
-    RED(0xFFFF0000),
-    ORANGE(0xFFFF5A59), // Cor fornecida no original
-    YELLOW(0xFFFFFF00),
-    GREEN(0xFF2FAC2B), // Cor fornecida no original
-    BLUE(0xFF2E6DEE), // Cor fornecida no original
-    INDIGO(0xFF4B0082),
-    VIOLET(0xFF914AC6) // Cor roxa/violeta semelhante à fornecida no original
+    RED(0xFFFF383C),
+    ORANGE(0xFFFF8D28),
+    YELLOW(0xFFFFCC00),
+    GREEN(0xFF34C759),
+    MINT(0xFF00C8B3),
+    BLUE(0xFF2E6DEE),
+    INDIGO(0xFF6155F5),
+    PURPLE(0xFFFF2D55),
+    BROWN(0xFFAC7F5E)
 }
