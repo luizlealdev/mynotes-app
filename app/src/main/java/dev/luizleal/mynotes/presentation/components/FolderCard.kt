6f1773889684bffcef2a83d35463fec1db1760ba
luizlealdev <luizandreleal100@gmail.com>
2025-12-02 18:48:52 -0300

    [feat]: create FolderCard component
    - The card shows a folder icon, the folder name, and its creation date.

---
+++ app/src/main/java/dev/luizleal/mynotes/presentation/components/FolderCard.kt
@@ -0,0 +1,81 @@
+package dev.luizleal.mynotes.presentation.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ShapeDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
+import androidx.compose.ui.unit.sp
+import dev.luizleal.mynotes.R
+import dev.luizleal.mynotes.domain.model.Folder
+import dev.luizleal.mynotes.domain.model.FolderColor
+import dev.luizleal.mynotes.util.formatTo
+import java.time.LocalDateTime
+
+@Composable
+fun FolderCard(
+    modifier: Modifier = Modifier,
+    folder: Folder,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = modifier
+            .clip(ShapeDefaults.Small)
+            .background(MaterialTheme.colorScheme.surface)
+            .clickable(onClick = onClick)
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(2.dp)
+    ) {
+
+        Image(
+            painter = painterResource(R.drawable.ui_folder),
+            contentDescription = folder.name,
+            modifier= Modifier.size(82.dp)
+        )
+        Text(
+            text = folder.name,
+            style = MaterialTheme.typography.bodyLarge.copy(
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Medium,
+            ),
+            overflow = TextOverflow.Ellipsis,
+        )
+        Spacer(modifier= Modifier.height(4.dp))
+        Text(
+            text = folder.createdAt?.formatTo() ?: "",
+            color = MaterialTheme.colorScheme.onSurfaceVariant,
+            style = MaterialTheme.typography.labelMedium.copy(
+                fontWeight = FontWeight.Normal
+            )
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun FolderCardPreview() {
+    FolderCard(
+        onClick = {},
+        folder = Folder(
+            name = "Estudos",
+            color = FolderColor.BLUE,
+            createdAt = LocalDateTime.now()
+        )
+    )
+}
